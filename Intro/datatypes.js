a39b60386312edc1ef9eb5641df39c94ea55bc90
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-06-17 15:43:31 +0300

    data types lesson

---
+++ Intro/datatypes.js
@@ -0,0 +1,55 @@
+//data types
+
+//numbers.
+//floating point;<decimal place>
+/*
+real numbers
+positive 
+negative number
+*/
+
+let number1=23; //real number
+console.log(number1)
+let number2=34.4; //floating point
+console.log(number2)
+let number3= -34 //negative number
+console.log(number3)
+
+/*
+Strings-> store sequence of characters
+3 ways of creating a string
+1.Double quote string
+2.single quote string
+3.back ticks
+*/
+
+let str1= "I am Bryan"
+console.log(str1)
+let str2='I am the greatest'
+console.log(str2)
+let str3=`leo mbuzi`
+console.log(str3)
+
+/*
+Boolean represents true or false
+*/
+let johnismale=true
+console.log(johnismale)
+let johnismarried=false
+console.log(johnismarried)
+
+/*
+null -> lack of value
+ */
+let nullvall=null
+console.log(nullvall)
+
+/*
+undefined-> lack of a value  i.e absence of a value
+diff between null and undifined
+
+ */
+let undefinedvall=undefined //dont do this
+console.log(undefined)
+let undefinedval2; //js engine set to undefined
+console.log(undefinedval2)
