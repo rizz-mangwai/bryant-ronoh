d5e99a2a9a80da91e421a1f9ef014a25acc7c1a3
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-06-18 15:54:42 +0300

    math operators and strings

---
+++ Intro/mathoperators.js
@@ -0,0 +1,36 @@
+/*
+assignment =
+addition + -> use to add two or more numbers
+multiplication * -> use to multiply 2 or more numbers
+division/
+-->powers
+ */
+let numb1=23; //declared and assigned the variable
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+numb1=40;// assignment only 
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+let numb2=60
+console.log(`numb2 is ${numb2} its type is${typeof numb2}`)
+let numb3=numb1+numb2+10
+console.log(`numb3 is ${numb3} its type is ${typeof numb3}`)
+let numb4=numb1+numb2+numb3+100
+console.log(`numb4 is ${numb4} its type is ${typeof numb4}`)
+let numb5=numb4-numb3+2*10
+console.log(`numb5 is ${numb5} its type is ${typeof numb5}`)
+let numb6=Math.pow(10,3)
+console.log(`numb6 is ${numb6} its type is${typeof numb6}`)
+//
+///
+let numb7=30
+let numb8=10
+let divnumb=numb7/numb8
+console.log(`divnumb is ${divnumb} its type is ${typeof divnumb}`)
+ // 
+ //modulus is somehow special
+ //divide 2 numbers -> remainder
+ let numb9=23
+ let numb10=12
+ let modNu=numb9%numb10
+ console.log(`modnu is ${modNu} its type is ${typeof modNu}`)
+ //calculation ->
+ //is a number x a multipule of 11
---
+++ Intro/templatestring.js
@@ -0,0 +1,16 @@
+/*
+
+ */
+let firstName="kijana"
+let secondname="uhuru"
+let age=20
+let address="eldoret"
+
+let userdetails=firstName+secondname+"is"+age+"years old"+"he stays at"+address
+console.log(userdetails)
+console.log(" user details is",typeof userdetails)
+///
+let userdetails2=
+`${firstName} ${secondname} is ${age} years old and stays at ${address}`
+console.log(userdetails2)
+console.log(`$firstname} ${secondname} is ${age} years old and he stay at ${address}`)
