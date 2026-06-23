6d781306acf9049449925265c995fa9c8853fcd8
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-06-23 08:54:21 +0300

    comparison operators

--- Intro/mathoperators.js
+++ Intro/mathoperators.js
@@ -34,3 +34,61 @@ console.log(`divnumb is ${divnumb} its type is ${typeof divnumb}`)
  console.log(`modnu is ${modNu} its type is ${typeof modNu}`)
  //calculation ->
  //is a number x a multipule of 11
+
+
+
+ let numb3=20;
+ let numb4=10
+
+ let str1="a"
+ let str2="b"
+
+ console.log(`num3=${numb3} and numb4=${numb4}`)
+ console.log(`str1=${str1} and str2=${str2}`)
+
+ console.log(`numb3>numb4 ${numb3>numb4}`)
+ console.log(`str2>str1 ${str1>str2}`)
+
+ console.log(`numb3>=numb4 ${numb3>=numb4}`)
+ console.log(`str2>=str1 ${str1>=str2}`)
+
+ console.log(`numb3<numb4 ${numb4.numb3}`)
+ console.log(`str2<str1 ${str1<str2}`)
+
+ console.log(`numb3<=numb4 ${numb3<=numb4}`)
+ console.log(`str2<=str1 ${str1<=str2}`)
+
+ 
+/*
+ comparison operators.
+Comparison operators evaluate to only true or false.*/
+/*
+And->
+truth table-> all posible combinations
+val1 and val2
+combinations are
+val1 && val2
+val2&&val1
+
+-> for and all statments have to be true for
+it to be true.
+*/
+let val1=true
+let val2=true
+let result1=val1&&val2
+console.log(`val1=${val1} val2=${val2} val1&val2=${val1&val2}`)
+
+let mercy_age=15
+let mercy_balance=50000
+//if mercy is age is greter than 18 and has more theo 20k in account
+let is_greater_than_18=mercy_age>18//true or felse
+console.log(`is mercy age greater than 18 $(is_greatar_than_18}`)
+let has_more_than_20k=mercy_balance>20000
+console.log(`Mercy has more than 20k ${has_more_than_20k}`)
+console.log(`mercy is age is greter than 18
+       and has more than 20k in her account
+${is_greater_than_18&&has_nore_than_20k}`)
+//mercy_age>18&&mercy_balance>20000
+let simplified=mercy_age>18&&mercy_balance>20000
+console.log(`simplified ${simplified}`)
+console.log(`further simplification is ${mercy_age>1&mercy_balance>20040}`)
