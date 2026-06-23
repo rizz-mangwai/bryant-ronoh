91730613fbb74335047c46a7f734c39584abd03b
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-06-23 15:21:36 +0300

    today's class

--- Intro/mathoperators.js
+++ Intro/mathoperators.js
@@ -59,36 +59,3 @@ console.log(`divnumb is ${divnumb} its type is ${typeof divnumb}`)
  console.log(`str2<=str1 ${str1<=str2}`)
 
  
-/*
- comparison operators.
-Comparison operators evaluate to only true or false.*/
-/*
-And->
-truth table-> all posible combinations
-val1 and val2
-combinations are
-val1 && val2
-val2&&val1
-
--> for and all statments have to be true for
-it to be true.
-*/
-let val1=true
-let val2=true
-let result1=val1&&val2
-console.log(`val1=${val1} val2=${val2} val1&val2=${val1&val2}`)
-
-let mercy_age=15
-let mercy_balance=50000
-//if mercy is age is greter than 18 and has more theo 20k in account
-let is_greater_than_18=mercy_age>18//true or felse
-console.log(`is mercy age greater than 18 $(is_greatar_than_18}`)
-let has_more_than_20k=mercy_balance>20000
-console.log(`Mercy has more than 20k ${has_more_than_20k}`)
-console.log(`mercy is age is greter than 18
-       and has more than 20k in her account
-${is_greater_than_18&&has_nore_than_20k}`)
-//mercy_age>18&&mercy_balance>20000
-let simplified=mercy_age>18&&mercy_balance>20000
-console.log(`simplified ${simplified}`)
-console.log(`further simplification is ${mercy_age>1&mercy_balance>20040}`)
---
+++ Intro/truthyandfalsey.js
@@ -0,0 +1,9 @@
+let username1="Bryan ronoh"//truthy
+let username2=0//falsey
+
+let activeusername=username2 ||username1
+//activeusername
+console.log(activeusername)
+
+let bothusernameok=username1&&username2
+console.log(bothusernameok)//truthy or falsey
