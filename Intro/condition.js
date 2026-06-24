46f2230ccbd1afbd721bafcefb5a475adfbb849f
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-06-24 11:12:18 +0300

    wednesday class

---
+++ Intro/condition.js
@@ -0,0 +1,29 @@
+let football="Messi is the goat"//23
+if(football){console.log("Messi is the goat")}
+
+
+//scene 2
+let Messi
+if (football){
+    console.log("football")
+}
+
+//scene 3
+if(true){
+    console.log("scenario 3 it run")
+}
+
+///scene 4
+if (false){
+    console.log("scenario 4 it run")
+}
+
+//scene 5
+if (10>7){
+    console.log("GOAT")
+}
+ 
+//scene 6
+if (7>10){
+    console.log(PUNNY)
+}
---
+++ Intro/if.js
@@ -0,0 +1,19 @@
+let age=prompt("enter your age:")
+alert(`you entered ${age}`)
+
+//age=23
+//23>10 true
+if (age>10){
+    alert("you are a baby")
+}
+//23>13 true
+if(age>13){alert (`youre a teen`)}
+//27>18=true
+if(age>18){alert(`you can smoke`)
+}
+//27>27 false
+if(age>27){alert(`best age to marry`)}
+//27>45 false
+if(age>45){alert("best age for marring again")}
+//27>50 false 
+if(age>50){alert('dying soon')}
