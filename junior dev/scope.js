82a663e719730c2ec887337826febe89e3146283
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-06-30 15:30:03 +0300

    scope

---
+++ junior dev/scope.js
@@ -0,0 +1,14 @@
+let name = "samson"
+function sayMyName(){
+    let name = "yobii"
+    console.log(`name:  ${name} line : 3`)
+if (true){
+    let name ="delilah"
+    console.log(`name: ${name} line:6`)
+} 
+console.log(`name: ${name} line: 10 `)
+
+}
+
+sayMyName()
+console.log(`name: ${name} line: 15`)
