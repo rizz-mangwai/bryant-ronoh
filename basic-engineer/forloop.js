e047e9520a721bc6465e7444803b42ce93b319a9
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-07-07 10:32:59 +0300

    while and for loop

---
+++ basic-engineer/forloop.js
@@ -0,0 +1,14 @@
+/*
+-->for loop -> mostly for numbers
+--combination of using while for number
+loops: 1: start point, 2:condition, 3: afteritteration
+combines it into one statement 
+*/
+let k = 0
+while(k < 20 ){
+    console.log("k", k)
+    k = k + 1
+}
+for (let k = 0; k < 20; k =k +1){
+    console.log("K", k)
+}
--- basic-engineer/whileloop.js
+++ basic-engineer/whileloop.js
@@ -19,3 +19,52 @@ while (condition){
         condition = false;
     }
 }
+
+///
+///
+function promptStudent(){
+    let num1= null
+    let num2 = null
+    while (true){
+        num1 = prompt("enter the first number");
+        if (isNaN(num1)){
+            continue;
+        }
+        num2 = prompt("enter the second name");
+        if (isNaN(num2)){
+            continue;//restart the loop
+        }
+        num1= parseInt(num1);
+        num2 = parseInt(num2);
+        if (num1 <= 0 || num2<=0){
+            continue;
+        }
+        break;
+    }
+    console.log("got valid number", num1, num2);
+    mathTable(num1 ,num2)
+}
+
+function mathTable(num1,num2){
+    if (!num1 || num1 < 0 || !num2 ||num2 < 0){
+        console.error("ensure numer 1 are numbers greater than 0")
+    return;
+    }
+let outerloop = num1
+while (outerloop > 0){
+    doInnerLoop(outerloop, num2);//
+    outerloop = outerloop - 1; 
+}
+//while(outerloop> o){
+// let innerloop = num2//3//3}
+//while (innerloop > 0){
+//console.log(`outerloo)}
+}
+function doInnerLoop(outerloop, num2){
+    let innerloop = num2;
+    while(innerloop > 0){
+        console.log(`outerloop=${outerloop} innerloop=${innerloop}`);
+        console.log(`${outerloop}*${innerloop}=${outerloop * innerloop}`);
+        innerloop - innerloop - 1
+    }
+}
