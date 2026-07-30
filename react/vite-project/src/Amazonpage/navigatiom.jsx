5f59124fe67f2e47c6358710dade1a7419587287
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-07-30 08:58:52 +0300

    vite-project

---
+++ react/vite-project/src/Amazonpage/navigatiom.jsx
@@ -0,0 +1,23 @@
+/*
+top nav of the page 
+
+*/
+/**
+ import assets
+ */
+import logo from "../assets/log.jpg";
+/*
+component naming rules
+return null or valid jsx
+*/
+function Navigation (){
+    return (
+        <div>
+            <img src={logo} width="50px" />
+            <input style={{}} />
+            <h2>navigation section</h2>
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ react/vite-project/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Navigation from "./Amazonpage/navigatiom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Navigation/>
+  </StrictMode>,
+)
